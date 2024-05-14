cfaa6384e293a0c83e37e14c04250ad66646ff1a
Mangiola <mass.mang@outlook.com>
2024-05-14 16:39:41 -0400

    Migrate to new repository

--- main.ts
+++ main.ts
@@ -1,134 +1,202 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownPostProcessorContext, MarkdownView, parseFrontMatterTags, Plugin, getAllTags, View, TFile, WorkspaceLeaf, TextFileView } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class TagStylesPlugin extends Plugin {
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	appliedClasses = new WeakMap<MarkdownView, string[]>();
 
 	async onload() {
-		await this.loadSettings();
+		// Processor that adds CSS class to a DIV with a tag in it
+		this.registerMarkdownPostProcessor(this.applyClass);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Processor that replaces <img> with <svg> when applicable
+		this.registerMarkdownPostProcessor((node, ctx) => {
+			const nodeEl = node as HTMLElement;
+			
+			nodeEl.findAll("p").forEach((pEl) => {
+				pEl.findAll("span").forEach(async (spanEl) => {
+					if (spanEl.hasAttribute("src") && spanEl?.getAttr("src")?.contains(".svg")) {	
+						const file = this.app.vault.getFiles().find((element) => element.name == spanEl.getAttr("src"));
+            if (!file)
+              return;
+						const content = await this.app.vault.cachedRead(file);
+						
+						const el = createEl("div");
+						el.innerHTML = content;
+						el.addClass("svg-div-embed");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+						if (spanEl.hasAttribute("alt")) {
+							pEl.parentElement?.setAttr("alt", spanEl.getAttr("alt"));
+						}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+						pEl.parentElement?.addClass("svg-container");
+						pEl.appendChild(el);
+						pEl.addClass("svg-p-embed");
+						pEl.removeChild(spanEl);
 					}
+				});
+			});
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		});
+
+		// Handler that adds CSS class to an entire page based on its tags
+		this.registerEvent(this.app.workspace.on('layout-change', this.handleLayoutChange.bind(this)));
+
+		this.registerMarkdownPostProcessor((element, context) => {
+
+		});
+	}	
+
+	onunload() { }
+
+	/**
+	 * Applies a CSS class to all tag elements in a markdown view. 
+	 */
+	applyClass(node: Node, ctx: MarkdownPostProcessorContext) {
+		const nodeEl = node as HTMLElement;
+		
+		// Apply a CSS class to all DIVs containing a tag
+		nodeEl.findAll("a.tag").forEach((tagEl) => {
+			const tag = (tagEl as HTMLAnchorElement).innerText
+        		.slice(1)
+        		.replace("/", "-");
+			nodeEl.addClass(`tag-${tag}`);
+		});	
+	}
+	
+
+	/**
+	 * On opening a file, apply a CSS class to it based on its tags and top-level folder.
+	 * Removes all the added classes when the file is closed.
+	 */
+	private handleLayoutChange() {
+		// Stop if there are no active views
+		const activeViews = this.getAllActiveViews();
+		if (!activeViews) {
+			return;
+		}
+
+		// Iterate each open markdown view
+		activeViews.forEach((view) => {
+			this.removePreviousClasses(view);
+			let container: Element | null;
+
+			// Make list of CSS classes to add
+			var classes: string[] = [];
+
+			// Add class for each tag
+      if (!view.file)
+        return;
+			const fileCache = this.app.metadataCache.getFileCache(view.file);
+      if (!fileCache)
+        return;
+			const fileTags = getAllTags(fileCache);
+      if (!fileTags)
+        return;
+			fileTags.forEach(function (element) {
+				classes.push("tag-"+element.replace("#",""))
+			});
+
+			// Add class for this file's current folder, if any
+			const filePath = view.file.path;
+			if (filePath.contains("/")) {
+				classes.push("tag-"+filePath.replace(" ", "").split("/")[0]);
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			if (this.isReadMode(view)) {
+				container = this.getPreviewContainer(view);
+			}
+			else if (this.isEditMode(view)) {
+				container = this.getEditContainer(view);
+			} else {
+        container = null;
+      }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+      if (!container)
+        return;
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			this.applyClasses(classes, view, container);
+		})
 	}
 
-	onunload() {
-
+	/**
+     * Get the active markdown view and any linked panes.
+     */
+	 private getAllActiveViews(): MarkdownView[] | null {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView) {
+		  // Get any linked views
+		  let activeViews: MarkdownView[] = [activeView];
+		  const leafGroup = this.app.workspace.getGroupLeaves((activeView.leaf as any).group);
+		  if (leafGroup && leafGroup.length > 0) {
+			activeViews = leafGroup
+			  .map((leaf) => leaf.view)
+			  .filter((view) => view instanceof MarkdownView) as MarkdownView[];
+		  }
+		  return activeViews;
+		}
+		return null;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	/**
+     * Given a view, remove any extra classes this plugin
+     * has applied to that view.
+     */
+	private removePreviousClasses(view: MarkdownView): void {
+		const previewContainer = this.getPreviewContainer(view);
+		const editContainer = this.getEditContainer(view);
+		const classes = this.appliedClasses.get(view);
+		if (classes && previewContainer) {
+		  previewContainer.removeClasses(classes);
+		}
+		if (classes && editContainer) {
+		  editContainer.removeClasses(classes);
+		}
+		this.appliedClasses.delete(view);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	/**
+     * Get the element that preview classes are applied to
+     */
+	private getPreviewContainer(view: MarkdownView) {
+		return view.contentEl.querySelector('.markdown-preview-view');
 	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	
+	/**
+	 * Get the element that edit/source classes are applied to
+	 */
+	private getEditContainer(view: MarkdownView) {
+		return view.contentEl.querySelector('.markdown-source-view');
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	/**
+     * Get the inline title element
+     */
+	private getTitleElement(view: MarkdownView) {
+		return view.contentEl.querySelector('.inline-title');
+	}
 
-		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	/**
+    * Returns true if a view is in preview mode
+    */
+	private isReadMode(view: MarkdownView): boolean {
+		return view.getMode() === 'preview';
+	}
+	
+	/**
+	 * Returns true if a view is in edit/source mode
+	 */
+	private isEditMode(view: MarkdownView): boolean {
+		return view.getMode() === 'source';
+	}
+
+	/**
+     * Add classes to an html element and store the
+     * applied classes along with a reference the view
+     * they were added to for removal later.
+     */
+	private applyClasses(classes: string[], view: MarkdownView, container: Element): void {
+		container.addClasses(classes);
+		this.appliedClasses.set(view, classes);
 	}
 }
